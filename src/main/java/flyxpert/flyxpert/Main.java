024127343819e5e2bc1ec730414dda03465f6439
Mohannadms <mohannadms2004@gmail.com>
2023-12-03 23:41:18 +0200

    Adding Search bar

--- src/main/java/flyxpert/flyxpert/Main.java
+++ src/main/java/flyxpert/flyxpert/Main.java
@@ -2,6 +2,7 @@ package flyxpert.flyxpert;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -17,7 +18,10 @@ public class Main extends Application {
     }
 
     public void start(Stage stage) throws IOException {
-        stage.setTitle("Hello!");
+
+        Parent root= FXMLLoader.load(getClass().getResource("SearchFlightPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
         stage.show();
     }
 }
