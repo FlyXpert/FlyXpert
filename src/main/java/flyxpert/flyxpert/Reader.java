54caeb4fab13f35fc48be8078ca98316740854f0
MostafaMohamed2005 <mostafa.mosael13@gmail.com>
2023-12-03 18:50:55 +0200

    Added classes for reading and writing

---
+++ src/main/java/flyxpert/flyxpert/Reader.java
@@ -0,0 +1,18 @@
+package flyxpert.flyxpert;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Reader {
+
+    public void readAllFiles(){
+        readAFile();
+    }
+
+    // This an example for reading data from a file
+    public void readAFile(){
+        //File file = new File("");
+        //Scanner scan = new Scanner(file);
+        //scan.close();
+    }
+}
---
+++ src/main/java/flyxpert/flyxpert/Writer.java
@@ -0,0 +1,19 @@
+package flyxpert.flyxpert;
+
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class Writer {
+    public void writeToAllFiles(){
+        writeToAFile();
+    }
+
+    // this is an example for writing your data to a file
+    public void writeToAFile(){
+        //PrintWriter writer = new PrintWriter("");
+        //writer.println();
+        //writer.close();
+    }
+
+
+}
