857176a3bb254ca349e0814841c74717553126f6
MostafaMohamed2005 <mostafa.mosael13@gmail.com>
2023-12-06 14:07:02 +0200

    VBox is not founded

--- src/main/java/flyxpert/flyxpert/FlightInformationController.java
+++ src/main/java/flyxpert/flyxpert/FlightInformationController.java
@@ -2,8 +2,11 @@ package flyxpert.flyxpert;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.File;
@@ -21,6 +24,37 @@ public class FlightInformationController implements Initializable {
         worldImageView.setImage(image);
     }
 
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox vbox;
 
+    public void fillDataOfFlights(){
 
+        try {
+               for(Flight flight : Flight.flights) {
+                   vbox.getChildren().add(createFlight(flight));
+               }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private HBox createFlight(Flight flight){
+        HBox hbox = new HBox();
+        hbox.setStyle("-fx-border-color: black; -fx-border-width: 0.5");
+
+        hbox.getChildren().add(new Label(flight.getAirlineName()));
+
+        hbox.getChildren().add(new Label(flight.getDepartureTime().getHour() + ":" + flight.getDepartureTime().getMinutes()
+                + "-" + flight.getArrivalTime().getHour() + ":" + flight.getArrivalTime().getMinutes()));
+
+        hbox.getChildren().add(new Label(flight.getArrivalDay().getDay()
+                + "-" + flight.getArrivalDay().getMonth()
+                + "-" + flight.getArrivalDay().getYear()));
+
+        hbox.getChildren().add(new Label(Integer.toString(flight.getEconomyPrice())));
+
+        return hbox;
+    }
 }
--- src/main/java/flyxpert/flyxpert/Main.java
+++ src/main/java/flyxpert/flyxpert/Main.java
@@ -20,8 +20,9 @@ public class Main extends Application {
     public static void main(String[] args) throws FileNotFoundException {
         Reader reader = new Reader();
         reader.readAllFiles();
-        System.out.println(Flight.flights.size());
-        launch(args);
+        FlightInformationController controller = new FlightInformationController();
+        controller.fillDataOfFlights();
+        //launch(args);
         Writer writer = new Writer();
         writer.writeToAllFiles();
     }
@@ -29,18 +30,7 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
         try{
-            Parent root= FXMLLoader.load(getClass().getResource("SearchFlightPage.fxml"));
-
-            
-            VBox vbox = new VBox();
-            //vbox.setSpacing(10);
-            //vbox.setPadding(new Insets(10));
-
-            for(Flight flight : Flight.flights){
-                vbox.getChildren().add(createFlight(flight));
-            }
-
-
+            Parent root = FXMLLoader.load(getClass().getResource("SearchFlightPage.fxml"));
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
             stage.setScene(scene);
@@ -52,21 +42,4 @@ public class Main extends Application {
         }
     }
 
-    private HBox createFlight(Flight flight){
-        HBox hbox = new HBox();
-        hbox.setStyle("-fx-border-color: black; -fx-border-width: 0.5");
-
-        hbox.getChildren().add(new Label(flight.getAirlineName()));
-
-        hbox.getChildren().add(new Label(flight.getDepartureTime().getHour() + ":" + flight.getDepartureTime().getMinutes()
-        + "-" + flight.getArrivalTime().getHour() + ":" + flight.getArrivalTime().getMinutes()));
-
-        hbox.getChildren().add(new Label(flight.getArrivalDay().getDay()
-        + "-" + flight.getArrivalDay().getMonth()
-        + "-" + flight.getArrivalDay().getYear()));
-
-        hbox.getChildren().add(new Label(Integer.toString(flight.getEconomyPrice())));
-
-        return hbox;
-    }
 }
