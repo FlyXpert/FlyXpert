2b267a74ece3c0ca409e34012a329bc231c199e5
MostafaMohamed2005 <mostafa.mosael13@gmail.com>
2023-12-07 14:14:47 +0200

    Added writeToFlightInformationFile method

--- src/main/java/flyxpert/flyxpert/Writer.java
+++ src/main/java/flyxpert/flyxpert/Writer.java
@@ -5,29 +5,29 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class Writer {
-    public void writeToAllFiles(){
-        writeToAFile();
+    public void writeToAllFiles() throws FileNotFoundException {
+        writeToFlightInformationFile();
     }
 
-    // This an example for writing data to a file (Remove this and add your own functions to write Users, Flights, etc.)
-    public void writeToAFile(){
-        // PrintWriter writer = new PrintWriter("");
-        // writer.println();
-        // Do writing here!
-        // writer.close();
-    }
-
-    /*public void writeToFlightInformationFile() throws FileNotFoundException {
+    public void writeToFlightInformationFile() throws FileNotFoundException {
 
         PrintWriter writer = new PrintWriter("FlightInformation.txt");
-
+        boolean firstElement = false;
         for(Flight flight : Flight.flights) {
+
+            if(firstElement)
+            writer.print(",");
+
             writer.println(flight.getDepartureAirport().getCode()+ "," + flight.getDepartureAirport().getName() + "," + flight.getDepartureAirport().getLocation() + ","
                     + flight.getArrivalAirport().getCode()+ "," + flight.getArrivalAirport().getName() + "," + flight.getArrivalAirport().getLocation() + ","
-                    + flight.getAirlineName()+ "," + flight.getDepartureTime() + "," +flight.getArrivalTime()+ "," + flight.getArrivalDay()+ ","
+                    + flight.getAirlineName()+ "," + flight.getDepartureTime().getHour() + "," +flight.getDepartureTime().getMinutes() + "," + flight.getDepartureTime().getPeriod() + ","
+                    + flight.getArrivalTime().getHour() + "," + flight.getArrivalTime().getMinutes() + "," + flight.getArrivalTime().getPeriod() + ","
+                    + flight.getArrivalDay().getDay() +  "," + flight.getArrivalDay().getMonth() +  "," + flight.getArrivalDay().getYear() +  ","
                     + flight.getEconomyPrice()+ "," + flight.getBusinessPrice() + "," + flight.getFirstClassPrice() + ","
                     + flight.getAvailableSeats()+ "," + flight.getFlightNumber());
+
+            firstElement = true;
         }
         writer.close();
-    }*/
+    }
 }
