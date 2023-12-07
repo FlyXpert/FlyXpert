060508e65401c0ce59bc4d19aee9e13741b71a46
Abdallah_Moe <am6691917@gmail.com>
2023-12-07 11:43:07 +0200

    Completed initial functionality,additional work needed for refinement

--- src/main/java/flyxpert/flyxpert/Main.java
+++ src/main/java/flyxpert/flyxpert/Main.java
@@ -2,13 +2,16 @@ package flyxpert.flyxpert;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
     public static void main(String[] args) {
+        BookingConfirmation bookingConfirmation = new BookingConfirmation("Mohsen", 123, "JAPAN", "patpal");
         Reader reader = new Reader();
         reader.readAllFiles();
         launch(args);
@@ -16,8 +19,17 @@ public class Main extends Application {
         writer.writeToAllFiles();
     }
 
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException
+    {
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("TestBooking.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TestBooking.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("MoeStyle.css").toExternalForm());
         stage.setTitle("Hello!");
+        stage.setResizable(false);
+
+        stage.setScene(scene);
         stage.show();
     }
 }
