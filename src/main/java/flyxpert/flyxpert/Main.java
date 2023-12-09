e92ef82ec64a1142ba1f5b01dd5ab141d4e42f43
Akayiz <ahmed.khaled.yousry@gmail.com>
2023-12-10 01:56:42 +0200

    Refactored the code and removed unnecessary stuff

--- src/main/java/flyxpert/flyxpert/Main.java
+++ src/main/java/flyxpert/flyxpert/Main.java
@@ -1,18 +1,12 @@
 package flyxpert.flyxpert;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import java.io.FileNotFoundException;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import java.io.IOException;
-
-
 public class Main extends Application {
     public static void main(String[] args) throws FileNotFoundException {
         Reader reader = new Reader();
-        System.out.println("$" + (200 + 300));
         reader.readAllFiles();
         ReadExternalConfig externalConfig = new ReadExternalConfig();
         launch(args);
