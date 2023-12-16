7b0caa63485d8a86aebd52a22cad109b3c62073c
MazeN Hassn <mazenby6@gmail.com>
2023-12-17 01:27:28 +0200

    Removed unnecessary comments

--- src/main/java/flyxpert/flyxpert/Dijkstra.java
+++ src/main/java/flyxpert/flyxpert/Dijkstra.java
@@ -73,25 +73,17 @@ public class Dijkstra
     }
     class Node implements Comparator<Node> {
 
-        // Member variables of this class
         public int node;
         public int cost;
 
-        // Constructors of this class
-
-        // Constructor 1
         public Node() {}
 
-        // Constructor 2
         public Node(int node, int cost)
         {
-
-            // This keyword refers to current instance itself
             this.node = node;
             this.cost = cost;
         }
 
-        // Method 1
         @Override
         public int compare(Node node1, Node node2)
         {
